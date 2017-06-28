18471a9be61753c57160a2360235b89ecbed07bf
yikwing <hubeieasy@gmail.com>
2017-06-28 14:54:34 +0800

    代码注释

--- app/src/main/java/com/example/rongyi/baseandroidmodel/MyApplication.kt
+++ app/src/main/java/com/example/rongyi/baseandroidmodel/MyApplication.kt
@@ -11,6 +11,17 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+
+        /**
+         *
+         * @Description: 根据调试/正式环境,显示/隐藏Debug日志输出
+         * @param:       if(true)显示所有日志
+         * @Data:        2017/6/28 下午2:51
+         * @author:      rongyi
+         * @return:
+         *
+         */
         if (BuildConfig.SHOW_LOG) {
             L.logLevel = L.LogLevel.DEBUG
         } else {
